678bf6a4aacfd28a94c821caa3e36d37a9cd4957
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-12-01 21:05:22 +0530

    Update userbotjoin.py

--- GAYU/utils/decorators/userbotjoin.py
+++ GAYU/utils/decorators/userbotjoin.py
@@ -8,9 +8,9 @@ from pyrogram.errors import (
     UserNotParticipant,
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import (
+from GAYU import app
+from GAYU.misc import SUDOERS
+from GAYU.utils.database import (
     get_assistant,
     get_lang,
     is_active_chat,
