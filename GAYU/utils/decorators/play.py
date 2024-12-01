42cb3073eae19a52488acc55604ee7a16f634d68
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-12-01 21:04:39 +0530

    Update play.py

--- GAYU/utils/decorators/play.py
+++ GAYU/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from SONALI import YouTube, app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import (
+from GAYU import YouTube, app
+from GAYU.misc import SUDOERS
+from GAYU.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from SONALI.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from SONALI.utils.inline import botplaylist_markup
+from GAYU.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
