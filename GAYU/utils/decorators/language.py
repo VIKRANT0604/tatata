3d082148bbdd0effac6219afee5afded386d25d1
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-12-01 21:03:27 +0530

    Update language.py

--- GAYU/utils/decorators/language.py
+++ GAYU/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import get_lang, is_maintenance
+from GAYU.misc import SUDOERS
+from GAYU.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
