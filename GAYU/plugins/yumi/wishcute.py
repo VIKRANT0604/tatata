b43549f078648eb8d4664bb9332e92ddf5b0e551
Samhere8412 <117311816+Samhere8412@users.noreply.github.com>
2025-01-16 23:45:01 +0530

    Update wishcute.py

--- GAYU/plugins/yumi/wishcute.py
+++ GAYU/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from GAYU import app 
 
-SUPPORT_CHAT = "New_sanatani"
+SUPPORT_CHAT = "FRIENDS ZONE"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
