19ccec20efecd4e8c1e5808521c69bfab8076811
Samhere8412 <117311816+Samhere8412@users.noreply.github.com>
2025-01-16 20:09:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","Unofficial_Legend")
+OWNER_USERNAME = getenv("OWNER_USERNAME","KingOfHellLuci")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","A_GRobot")
+BOT_USERNAME = getenv("BOT_USERNAME","Buggu_Music_Bot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
-MUST_JOIN  = "creating_77"
+MUST_JOIN  = "FRIENDS_ZONE_CHATTING_GROUP"
 # ---------------------------------------------------------
 
 # Get your mongo url from cloud.mongodb.com
@@ -31,7 +31,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1700))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7291963092))
+OWNER_ID = int(getenv("OWNER_ID", 2073178010))
 
 
 ## Fill these variables if you're deploying on heroku.
@@ -49,8 +49,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/creating_77")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/creating_77")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FRIENDS_ZONE_CHATTING_GROUP")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FRIENDS_ZONE_CHATTING_GROUP")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
